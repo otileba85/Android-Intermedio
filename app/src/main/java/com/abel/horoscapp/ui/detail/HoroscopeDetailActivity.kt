9f2fe85a1a13387c62690cc68925bf3312b0f658
otileba85 <otileba85@gmail.com>
2024-02-06 11:18:52 -0500

    added Animation added HoroscopeDetailActivity.kt

---
+++ app/src/main/java/com/abel/horoscapp/ui/detail/HoroscopeDetailActivity.kt
@@ -0,0 +1,14 @@
+package com.abel.horoscapp.ui.detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.abel.horoscapp.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HoroscopeDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_horoscope_detail)
+    }
+}
--- app/src/main/java/com/abel/horoscapp/ui/horoscope/adapter/HoroscopeAdapter.kt
+++ app/src/main/java/com/abel/horoscapp/ui/horoscope/adapter/HoroscopeAdapter.kt
@@ -6,7 +6,8 @@ import androidx.recyclerview.widget.RecyclerView
 import com.abel.horoscapp.R
 import com.abel.horoscapp.domain.model.HoroscopeInfo
 
-class HoroscopeAdapter(private var horoscopelist: List<HoroscopeInfo> = emptyList()) :
+class HoroscopeAdapter(private var horoscopelist: List<HoroscopeInfo> = emptyList(),
+    private val onItemSelected:(HoroscopeInfo)-> Unit) :
     RecyclerView.Adapter<HoroscopeViewHolder>() {
 
         fun updateList(list:List<HoroscopeInfo>){
@@ -23,7 +24,7 @@ class HoroscopeAdapter(private var horoscopelist: List<HoroscopeInfo> = emptyLis
     override fun getItemCount() = horoscopelist.size
 
     override fun onBindViewHolder(holder: HoroscopeViewHolder, position: Int) {
-        holder.render(horoscopelist[position])
+        holder.render(horoscopelist[position], onItemSelected)
     }
 
 }
