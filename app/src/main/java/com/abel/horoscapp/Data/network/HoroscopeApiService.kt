1d858293371cf1401a61fc1388d85946c978f7a7
otileba85 <otileba85@gmail.com>
2024-02-09 16:53:25 -0500

    added HoroscopeApiService.kt

--- app/src/main/java/com/abel/horoscapp/Data/network/HoroscopeApiService.kt
+++ app/src/main/java/com/abel/horoscapp/Data/network/HoroscopeApiService.kt
@@ -1,4 +1,10 @@
 package com.abel.horoscapp.Data.network
 
+import com.abel.horoscapp.Data.network.response.PredictionResponse
+import retrofit2.http.GET
+import retrofit2.http.Path
+
 interface HoroscopeApiService {
+    @GET("/{sign}")
+    suspend fun getHoroscope(@Path("sign") sign:String):PredictionResponse
 }
---
+++ app/src/main/java/com/abel/horoscapp/domain/Repository.kt
@@ -0,0 +1,4 @@
+package com.abel.horoscapp.domain
+
+interface Repository {
+}
